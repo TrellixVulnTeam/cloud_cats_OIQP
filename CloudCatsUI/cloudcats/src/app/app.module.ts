0c4ebf2bd04ba60fbee3debf9c4807cbcaac0fd1
sujithbaktha <71792626+sujithbaktha@users.noreply.github.com>
2022-05-13 12:39:25 -0500

    filter

--- CloudCatsUI/cloudcats/src/app/app.module.ts
+++ CloudCatsUI/cloudcats/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LaunchComponent } from './launch/launch.component';
 import { RouterModule } from '@angular/router';
@@ -15,7 +15,8 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     HttpClientModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
     
   ],
   providers: [],
--- CloudCatsUI/cloudcats/src/app/launch/launch.component.ts
+++ CloudCatsUI/cloudcats/src/app/launch/launch.component.ts
@@ -10,15 +10,34 @@ import { Subscription } from 'rxjs';
 })
 export class LaunchComponent implements OnInit, OnDestroy {
   
-  places: IList[] = [];
+  
   errorMessage = '';
   sub!: Subscription;
+
+  private _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredList = this.performFilter(value);
+  }
+  places: IList[] = [];
+  filteredList: IList[] = [];
+
   constructor(private launchService: LaunchService) { }
+
+  performFilter(filterBy: string): IList[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.places.filter((place: IList) =>
+      place.businessName.toLocaleLowerCase().includes(filterBy));
+  }
 
   ngOnInit(): void {
     this.sub = this.launchService.getLists().subscribe({
       next: places => {
         this.places = places;
+        this.filteredList = this.places;
       },
       error: err => this.errorMessage = err
     });
