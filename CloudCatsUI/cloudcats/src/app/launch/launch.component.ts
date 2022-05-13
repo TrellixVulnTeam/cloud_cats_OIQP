3e1faa2a77bdfe32f3647e7ecfc671a2b98d9f87
sujithbaktha <71792626+sujithbaktha@users.noreply.github.com>
2022-05-13 09:53:23 -0500

    mock data

--- CloudCatsUI/cloudcats/src/app/launch/launch.component.ts
+++ CloudCatsUI/cloudcats/src/app/launch/launch.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IList } from './launch';
+import { LaunchService } from './launch.http.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-launch',
   templateUrl: './launch.component.html',
   styleUrls: ['./launch.component.css']
 })
-export class LaunchComponent implements OnInit {
-  public pageTitle = 'Cloud Cats: We Hear You Cloud & Clear';
-  constructor() { }
+export class LaunchComponent implements OnInit, OnDestroy {
+  
+  places: IList[] = [];
+  errorMessage = '';
+  sub!: Subscription;
+  constructor(private launchService: LaunchService) { }
 
   ngOnInit(): void {
+    this.sub = this.launchService.getLists().subscribe({
+      next: places => {
+        this.places = places;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
