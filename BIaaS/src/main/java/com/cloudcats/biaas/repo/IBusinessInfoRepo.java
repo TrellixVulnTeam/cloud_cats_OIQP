64c5c772f9a819830a04e7c71189872d450d249b
Ajit Thakur <athakur0923@gmail.com>
2022-05-16 11:36:37 -0500

    updates for search or filter functionality

--- BIaaS/src/main/java/com/cloudcats/biaas/repo/IBusinessInfoRepo.java
+++ BIaaS/src/main/java/com/cloudcats/biaas/repo/IBusinessInfoRepo.java
@@ -2,15 +2,15 @@ package com.cloudcats.biaas.repo;
 
 import com.cloudcats.biaas.entity.BusinessInfo;
 import org.springframework.cloud.gcp.data.spanner.repository.SpannerRepository;
+import org.springframework.cloud.gcp.data.spanner.repository.query.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface IBusinessInfoRepo extends SpannerRepository<BusinessInfo, Integer> {
-    List<BusinessInfo> findAll();
-
-    @Override
-    Optional<BusinessInfo> findById(Integer integer);
+    List<BusinessInfo> findBusinessInfosByBusinessName(String businessName);
 }
